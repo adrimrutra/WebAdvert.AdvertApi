27eeb958a7af58c7141a81a28e54a86fbd8537b0
Artur Mirda <adrimrutra@gmail.com>
2020-02-08 19:50:02 +0200

    Microservice add

---
+++ AdvertApi/Services/AdvertProfile.cs
@@ -0,0 +1,14 @@
+﻿using AdvertApi.Models;
+using AutoMapper;
+using WebAdvert.Models;
+
+namespace AdvertApi.Services
+{
+    public class AdvertProfile : Profile
+    {
+        public AdvertProfile ()
+        {
+            CreateMap<AdvertModel, AdvertDbModel>();
+        }
+    }
+}
---
+++ AdvertApi/Services/DynamoDbAdvertStorage.cs
@@ -0,0 +1,63 @@
+﻿using AdvertApi.Interfaces;
+using AdvertApi.Models;
+using AutoMapper;
+using System;
+using System.Threading.Tasks;
+using WebAdvert.Models;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using System.Collections.Generic;
+
+namespace AdvertApi.Services
+{
+    public class DynamoDbAdvertStorage : IAdvertStorageService
+    {
+        private readonly IMapper _mapper;
+
+        public DynamoDbAdvertStorage(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<string> Add(AdvertModel model)
+        {
+            var dbModel = _mapper.Map<AdvertDbModel>(model);
+            dbModel.Id = new Guid().ToString();
+            dbModel.CreationDateTime = DateTime.Now;
+            dbModel.Status = AdvertStatus.Pending;
+
+            using (var client = new AmazonDynamoDBClient())
+            {
+                using (var context = new DynamoDBContext(client))
+                {
+                    await context.SaveAsync(dbModel);
+                }
+            }
+            return dbModel.Id;
+        }
+
+        public async Task Confirm(ConfirmAdvertModel model)
+        {
+            using (var client = new AmazonDynamoDBClient())
+            {
+                using (var context = new DynamoDBContext(client))
+                {
+                    var record = await context.LoadAsync<AdvertDbModel>(model.Id);
+                    if (record == null)
+                    {
+                        throw new KeyNotFoundException($"A record with ID={model.Id} was not found.");
+                    }
+                    if(model.Status == AdvertStatus.Active)
+                    {
+                        record.Status = AdvertStatus.Active;
+                        await context.SaveAsync(record);
+                    }
+                    else
+                    {
+                        await context.DeleteAsync(record);
+                    }
+                }
+            }
+        }
+    }
+}
